c21df036638d2bab3131b1bc550927ffe6e9e11f
mmdanish <mmdanish3321@gmail.com>
2025-05-02 10:03:20 +0530

    responsive bug fixes

--- src/pages/Admin Tenants/TenantsMaster/TenantsMaster.jsx
+++ src/pages/Admin Tenants/TenantsMaster/TenantsMaster.jsx
@@ -310,7 +310,7 @@ const TenantsMaster = () => {
           </tbody>
         </table>
       </div>
-      <div className="flex flex-col md:flex-row justify-between items-start md:items-center p-3 md:p-5 pagination-container">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center p-3 md:px-5 pagination-container">
         <span className="collection-list-pagination pagination-text">
           Showing{" "}
           {Math.min((currentPage - 1) * itemsPerPage + 1, filteredData.length)}{" "}
