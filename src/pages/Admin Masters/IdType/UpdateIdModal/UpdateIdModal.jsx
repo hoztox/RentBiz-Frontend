e685bb8b52a1dfbf78d4ae485cf4aa9f399ef95f
mmdanish <mmdanish3321@gmail.com>
2025-05-03 12:15:43 +0530

    UpdateIdModal Responsive

--- src/pages/Admin Masters/IdType/UpdateIdModal/UpdateIdModal.jsx
+++ src/pages/Admin Masters/IdType/UpdateIdModal/UpdateIdModal.jsx
@@ -22,12 +22,12 @@ const UpdateIdModal = ({ isOpen, onClose, idtype }) => {
 
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white rounded-md w-[522px] h-[262px] p-6 relative">
-        <h2 className="modal-head mt-4 mb-6">Create New ID Type Master</h2>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 modal-overlay">
+      <div className="update-id-modal-container relative bg-white rounded-md w-full max-w-[522px] h-auto md:h-[262px] p-6">
+        <h2 className="modal-head mt-4 mb-6">Update ID Type Master</h2>
         <button
           onClick={onClose}
-          className="absolute top-6 right-6 close-btn mt-[9px]"
+          className="absolute top-6 right-6 close-btn duration-200"
         >
           <img src={closeicon} alt="close" className="w-4 h-4" />
         </button>
@@ -36,7 +36,7 @@ const UpdateIdModal = ({ isOpen, onClose, idtype }) => {
           <label className="block pt-2 modal-label">Name</label>
           <input
             type="text"
-            className="w-full border border-[#E9E9E9] rounded-md mt-1 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            className="w-full border border-[#E9E9E9] rounded-md mt-1 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500 input-style"
             placeholder=""
             value={name}
             onChange={(e) => setName(e.target.value)}
@@ -46,7 +46,7 @@ const UpdateIdModal = ({ isOpen, onClose, idtype }) => {
         <div className="flex justify-end">
           <button
             onClick={handleUpdate}
-            className="bg-[#2892CE] hover:bg-[#2276a7] text-white rounded w-[150px] h-[38px] modal-save-btn"
+            className="bg-[#2892CE] hover:bg-[#2276a7] text-white rounded w-[150px] h-[38px] modal-save-btn duration-200"
           >
             Save
           </button>
