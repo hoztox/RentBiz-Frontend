9963a899663d61c82a9f6b2d7d51ae166c998f34
mmdanish <mmdanish3321@gmail.com>
2025-05-03 09:57:23 +0530

    createUnitTypeModal Responsive

--- src/pages/Admin Masters/UnitType/CreateUnitTypeModal/CreateUnitTypeModal.jsx
+++ src/pages/Admin Masters/UnitType/CreateUnitTypeModal/CreateUnitTypeModal.jsx
@@ -3,30 +3,28 @@ import "./CreateUnitTypeModal.css";
 import closeicon from "../../../../assets/Images/Admin Masters/close-icon.svg";
 
 const CreateUnitTypeModal = ({ isOpen, onClose }) => {
-    const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
-    const handleSave = () => {
-        if (name) {
-            console.log("New Unit Type Created: ", name);
-            onClose();
-        }
+  const handleSave = () => {
+    if (name) {
+      console.log("New Unit Type Created: ", name);
+      onClose();
     }
+  };
 
-      useEffect(() => {
-        if (isOpen) {
-          setName("");
-        }
-      }, [isOpen]);
+  useEffect(() => {
+    if (isOpen) {
+      setName("");
+    }
+  }, [isOpen]);
 
-    if(!isOpen) return null;
+  if (!isOpen) return null;
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white rounded-md w-[522px] h-[262px] p-6 relative">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 modal-overlay">
+      <div className="create-unit-modal-container relative bg-white rounded-md w-full max-w-[522px] h-auto md:h-[262px] p-6">
         <h2 className="modal-head mt-4 mb-6">Create New Unit Type Master</h2>
-        <button
-          onClick={onClose}
-          className="absolute top-6 right-6 close-btn mt-[9px]"
-        >
+        <button onClick={onClose} className="absolute top-6 right-6 close-btn">
           <img src={closeicon} alt="close" className="w-4 h-4" />
         </button>
 
@@ -34,7 +32,7 @@ const CreateUnitTypeModal = ({ isOpen, onClose }) => {
           <label className="block pt-2 modal-label">Name</label>
           <input
             type="text"
-            className="w-full border border-[#E9E9E9] rounded-md mt-1 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            className="w-full border border-[#E9E9E9] rounded-md mt-1 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500 input-style"
             placeholder="Enter Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
