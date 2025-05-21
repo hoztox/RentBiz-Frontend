c4fe0989c613cb8c472403149bd2b6a00a61d79b
mmdanish <mhddanish567@gmail.com>
2025-05-21 14:08:43 +0530

    ResponsiveDocumentForm style

--- src/pages/Admin Properties/Buildings/Add Building Modal/Upload Documents/ResponsiveDocumentForm.jsx
+++ src/pages/Admin Properties/Buildings/Add Building Modal/Upload Documents/ResponsiveDocumentForm.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
-import { ChevronDown, Calendar, Paperclip, Eye, X } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 import DocumentUploadModal from "./DocumentUploadModal/DocumentUploadModal";
 import "./documentform.css";
 import { useNavigate } from "react-router-dom";
+import documentIcon from "../../../../../assets/Images/Admin Units/document-icon.svg";
+import viewIcon from "../../../../../assets/Images/Admin Buildings/viewfile-icon.svg";
+import closeIcon from "../../../../../assets/Images/Admin Units/close-icon-white.svg";
+import plusIcon from "../../../../../assets/Images/Admin Units/plus-icon-black.svg";
 
 const ResponsiveDocumentForm = () => {
   const [documents, setDocuments] = useState([
     {
       id: 1,
-      type: "Permit",
+      type: "",
       number: "0123456789",
       issueDate: "10/01/2024",
       expiryDate: "10/01/2024",
@@ -36,7 +40,11 @@ const ResponsiveDocumentForm = () => {
   };
 
   const handleRemoveDocument = (id) => {
-    setDocuments(documents.filter((doc) => doc.id !== id));
+    if (documents.length > 1) {
+      setDocuments(documents.filter((doc) => doc.id !== id));
+    } else {
+      console.log("Cannot remove the last document set.");
+    }
   };
 
   const handleChange = (id, field, value) => {
@@ -53,10 +61,16 @@ const ResponsiveDocumentForm = () => {
     setIsModalOpen(false);
     console.log("Form submitted with documents:", documents);
     // Update completedSteps to include "Upload Documents" (id: 2) and "Submitted" (id: 3)
-    const currentCompletedSteps = JSON.parse(localStorage.getItem('completedSteps')) || [];
-    const updatedCompletedSteps = [...new Set([...currentCompletedSteps, 2, 3])]; // Add ids 2 and 3
-    localStorage.setItem('completedSteps', JSON.stringify(updatedCompletedSteps));
-    localStorage.setItem('activeCard', 'null'); // No active card after submission
+    const currentCompletedSteps =
+      JSON.parse(localStorage.getItem("completedSteps")) || [];
+    const updatedCompletedSteps = [
+      ...new Set([...currentCompletedSteps, 2, 3]),
+    ]; // Add ids 2 and 3
+    localStorage.setItem(
+      "completedSteps",
+      JSON.stringify(updatedCompletedSteps)
+    );
+    localStorage.setItem("activeCard", "null"); // No active card after submission
     navigate("/admin/submitted"); // Navigate to SubmissionConfirmationResponsive
   };
 
@@ -72,17 +86,17 @@ const ResponsiveDocumentForm = () => {
       <div className="flex-1 flex flex-col">
         <div className="flex-1 overflow-y-auto">
           {documents.map((doc) => (
-            <div key={doc.id} className="border-b first:pt-0 py-5">
+            <div key={doc.id} className="first:pt-0 py-5">
               <div>
-                {/* Row 1: DocType & Number */}
+                {/* Row 1: Doc.Type & Number */}
                 <div className="grid grid-cols-2 gap-3 mb-3 md:grid-cols-5 md:gap-5">
                   <div className="col-span-1 md:col-span-1">
-                    <label className="block text-sm text-gray-800 mb-2">
+                    <label className="block documents-label mb-2">
                       Doc.Type
                     </label>
                     <div className="relative">
                       <select
-                        className="appearance-none w-full p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50 cursor-pointer"
+                        className="appearance-none documents-inputs w-full p-3 border border-gray-200 rounded-md text-gray-500 cursor-pointer"
                         value={doc.type}
                         onChange={(e) =>
                           handleChange(doc.id, "type", e.target.value)
@@ -99,17 +113,17 @@ const ResponsiveDocumentForm = () => {
                   {doc.type && (
                     <>
                       <div className="col-span-1 md:col-span-1">
-                        <label className="block text-sm text-gray-800 mb-2">
+                        <label className="block documents-label mb-2">
                           Number
                         </label>
                         <input
                           type="text"
-                          className="w-full p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50 md:w-full"
+                          className="w-full p-3 border border-gray-200 rounded-md text-gray-500 md:w-full documents-inputs"
                           value={doc.number}
                           onChange={(e) =>
                             handleChange(doc.id, "number", e.target.value)
                           }
-                          placeholder="Number"
+                          placeholder="0123456789"
                         />
                       </div>
                       <div className="md:col-span-3"></div>
@@ -121,37 +135,35 @@ const ResponsiveDocumentForm = () => {
                     {/* Row 2: Issue Date & Expiry Date */}
                     <div className="grid grid-cols-2 gap-3 mb-3 md:grid-cols-5 md:gap-5">
                       <div className="col-span-1 md:col-span-1">
-                        <label className="block text-sm text-gray-800 mb-2">
+                        <label className="block documents-label mb-2">
                           Issue Date
                         </label>
                         <div className="relative">
                           <input
-                            type="text"
-                            className="w-full p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50 md:w-full"
+                            type="date"
+                            className="documents-inputs w-full p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50 md:w-full"
                             value={doc.issueDate}
                             onChange={(e) =>
                               handleChange(doc.id, "issueDate", e.target.value)
                             }
                             placeholder="MM/DD/YYYY"
                           />
-                          <Calendar className="absolute right-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-500" />
                         </div>
                       </div>
                       <div className="col-span-1 md:col-span-1">
-                        <label className="block text-sm text-gray-800 mb-2">
+                        <label className="block documents-label mb-2">
                           Expiry Date
                         </label>
                         <div className="relative">
                           <input
-                            type="text"
-                            className="w-full p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50 md:w-full"
+                            type="date"
+                            className="documents-inputs w-full p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50 md:w-full"
                             value={doc.expiryDate}
                             onChange={(e) =>
                               handleChange(doc.id, "expiryDate", e.target.value)
                             }
                             placeholder="MM/DD/YYYY"
                           />
-                          <Calendar className="absolute right-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-500" />
                         </div>
                       </div>
                       <div className="md:col-span-3"></div>
@@ -159,13 +171,13 @@ const ResponsiveDocumentForm = () => {
                     {/* Row 3: Upload Files & View Doc with Close Button */}
                     <div className="grid grid-cols-2 gap-3 md:grid-cols-5 md:gap-5">
                       <div className="col-span-1 md:col-span-1">
-                        <label className="block text-sm text-gray-800 mb-2">
-                          Upload Files
-                        </label>
                         <div className="relative">
+                          <label className="block documents-label">
+                            Upload Files
+                          </label>
                           <input
                             type="file"
-                            className="hidden"
+                            className="hidden documents-inputs"
                             id={`fileInput-${doc.id}`}
                             multiple
                             onChange={(e) =>
@@ -178,33 +190,47 @@ const ResponsiveDocumentForm = () => {
                           />
                           <label
                             htmlFor={`fileInput-${doc.id}`}
-                            className="flex items-center justify-between w-full p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50 cursor-pointer md:w-full"
+                            className="flex items-center justify-between documents-inputs cursor-pointer w-[161px] !py-2"
                           >
-                            <span className="text-sm truncate">
-                              Attach Files
+                            <span className="text-[#4B465C60] text-sm truncate">
+                              {doc.files.length > 0
+                                ? `${doc.files.length} file(s)`
+                                : "Attach Files"}
                             </span>
-                            <Paperclip className="h-4 w-4 text-gray-500" />
+                            <img
+                              src={documentIcon}
+                              alt="attach"
+                              className="ml-2 h-5 w-5 files-icon"
+                            />
                           </label>
                         </div>
                       </div>
                       <div className="col-span-1 md:col-span-1">
-                        <label className="block text-sm text-gray-800 mb-2">
+                        <label className="block documents-label mb-2">
                           View Doc
                         </label>
                         <div className="flex gap-2">
                           <div
-                            className="flex items-center justify-between flex-grow p-3 border border-gray-200 rounded-md text-gray-500 bg-gray-50"
+                            className="flex items-center justify-between flex-grow p-3 border border-gray-200 rounded-md text-gray-500 documents-inputs"
                             style={{ width: "calc(100% - 40px)" }}
                           >
                             <span className="text-sm">View File</span>
-                            <Eye className="h-4 w-4 text-gray-500" />
+                            <img
+                              src={viewIcon}
+                              alt="view"
+                              className="ml-2 h-5 w-5 files-icon"
+                            />
                           </div>
                           <button
                             type="button"
                             onClick={() => handleRemoveDocument(doc.id)}
-                            className="p-3 bg-red-500 hover:bg-red-600 rounded-md text-white flex justify-center items-center"
+                            className="p-2 bg-[#E44747] hover:bg-[#d43939] res-remove-btn flex justify-center items-center duration-200"
                           >
-                            <X className="h-4 w-4" />
+                            <img
+                              src={closeIcon}
+                              className="h-4 w-4"
+                              alt="remove"
+                            />
                           </button>
                         </div>
                       </div>
@@ -215,19 +241,22 @@ const ResponsiveDocumentForm = () => {
               </div>
             </div>
           ))}
-          {/* Button Section */}
-          <div className="flex flex-col pt-6">
-            <div className="flex justify-center mb-4">
+          {/* Add Document Button */}
+          <div className="pt-0 pb-4 border-b">
+            <div className="flex justify-end">
               <button
                 type="button"
                 onClick={handleAddDocument}
-                className="w-full py-3 px-4 border border-gray-800 rounded-md text-gray-800 hover:bg-gray-800 hover:text-white flex items-center justify-center transition-colors"
+                className="inline-flex justify-center items-center px-3 py-2 text-[#201D1E] bg-white hover:bg-[#201D1E] hover:text-white res-add-btn duration-200"
               >
                 Add New Document Set
-                <span className="ml-2 text-lg">+</span>
+                <img src={plusIcon} className="ml-1 h-5 w-5 add-icon" alt="add" />
               </button>
             </div>
-            <div className="grid grid-cols-2 gap-4">
+          </div>
+          {/* Bottom Button Section */}
+          <div className="pt-6">
+            <div className="grid grid-cols-2 gap-4 mb-[80px]">
               <button
                 type="button"
                 className="text-[#201D1E] bg-white hover:bg-[#201D1E] hover:text-white back-button duration-200"
@@ -244,13 +273,13 @@ const ResponsiveDocumentForm = () => {
             </div>
           </div>
         </div>
+        <DocumentUploadModal
+          isOpen={isModalOpen}
+          onCancel={handleCancelModal}
+          onConfirm={handleConfirmModal}
+          documents={modalDocuments}
+        />
       </div>
-      <DocumentUploadModal
-        isOpen={isModalOpen}
-        onCancel={handleCancelModal}
-        onConfirm={handleConfirmModal}
-        documents={modalDocuments}
-      />
     </div>
   );
 };
