f8dad5061f1659fb46f99d6ce304217872d4bd3e
mmdanish <mhddanish567@gmail.com>
2025-06-02 17:33:35 +0530

    feat(AddBuilding): Responsive navbar dynamic

--- src/pages/Admin Properties/Buildings/Add Building Modal/Submit/SubmissionConfirmation.jsx
+++ src/pages/Admin Properties/Buildings/Add Building Modal/Submit/SubmissionConfirmation.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./submissionconfirmation.css";
 import success from "../../../../../assets/Images/Admin Buildings/success.png";
 import successtick from "../../../../../assets/Images/Admin Buildings/success_tick.png";
 
-const SubmissionConfirmation = () => {
+const SubmissionConfirmation = ({onClose}) => {
+  useEffect(() => {
+    // Check if the screen width is below 480px (mobile view)
+    const isMobile = window.matchMedia("(max-width: 480px)").matches;
+
+    if (isMobile) {
+      // Set a timeout to call onClose after 3 seconds (3000ms)
+      const timer = setTimeout(() => {
+        onClose();
+      }, 5000);
+
+      // Cleanup the timer when the component unmounts
+      return () => clearTimeout(timer);
+    }
+  }, [onClose]);
+
   return (
     <div className="submit-confirm-container">
       <div className="submit-confirm-box">
