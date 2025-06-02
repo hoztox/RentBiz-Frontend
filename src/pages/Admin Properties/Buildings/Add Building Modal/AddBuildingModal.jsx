3d74324081bffca533536877561454828bae31d5
Mohammed Afeef NK <connnectafeefnk@gmail.com>
2025-06-02 08:37:53 +0530

    refactor(Building) implement new responsive design for buildiing

--- src/pages/Admin Properties/Buildings/Add Building Modal/AddBuildingModal.jsx
+++ src/pages/Admin Properties/Buildings/Add Building Modal/AddBuildingModal.jsx
@@ -17,17 +17,17 @@ const AddBuildingModal = ({ open, onClose, onBuildingCreated }) => {
         >
             <div
                 onClick={(e) => e.stopPropagation()}
-                className={`max-[480px]:relative building-modal-styles transition-all
+                className={`max-[480px]:relative building-modal-styles transition-all top-[90px]
                 ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}
             >
                 {/* Mobile Image */}
-                <div className='min-[480px]:hidden w-full h-[117px] bg-[#1458A2] flex justify-center relative'>
+                <div className='min-[480px]:hidden w-full h-[117px] bg-[#1458A2] flex justify-center relative top-[-42px]'>
                     <img src={buildingimg} alt="Building Top" className='top-[17px] absolute w-[85%]' />
                 </div>
 
                 {/* Mobile Header Bar */}
                 <div
-                    className="min-[480px]:hidden bg-white w-full h-[54px] mx-[16px] rounded-md absolute max-w-[calc(100vw-32px)] top-[88px] p-3 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] flex items-center justify-between cursor-pointer z-10"
+                    className="min-[480px]:hidden bg-white w-full h-[54px] mx-[16px] rounded-md absolute max-w-[calc(100vw-32px)] top-[40px] p-3 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] flex items-center justify-between cursor-pointer z-10"
                     onClick={handleToggle}
                 >
                     <div className="flex items-center space-x-5">
